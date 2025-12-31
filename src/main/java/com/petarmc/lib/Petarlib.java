fd95752d095f54dc61a05a0166011a2219d9a491
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2025-12-31 18:32:28 +0200

    feat: implement ClientModInitializer for Petarlib initialization

--- src/main/java/com/petarmc/lib/Petarlib.java
+++ src/main/java/com/petarmc/lib/Petarlib.java
@@ -2,12 +2,14 @@ package com.petarmc.lib;
 
 import com.petarmc.lib.log.PLog;
 import com.petarmc.lib.log.PLogger;
+import net.fabricmc.api.ClientModInitializer;
 
-public class Petarlib {
+public class Petarlib implements ClientModInitializer {
 
     private static final PLogger log = new PLog("Petarlib");
 
-    public static void init() {
+    @Override
+    public void onInitializeClient() {
         log.info("PetarLib initializing");
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
