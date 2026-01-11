e5d882d883f2a03a63735b5a94406569fdfb7958
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2026-01-11 18:57:52 +0200

    feat: set global log prefix for Petarlib initialization

--- src/main/java/com/petarmc/lib/Petarlib.java
+++ src/main/java/com/petarmc/lib/Petarlib.java
@@ -2,14 +2,17 @@ package com.petarmc.lib;
 
 import com.petarmc.lib.log.PLog;
 import com.petarmc.lib.log.PLogger;
+import com.petarmc.lib.log.LogConfig;
 import net.fabricmc.api.ClientModInitializer;
 
 public class Petarlib implements ClientModInitializer {
 
     private static final PLogger log = new PLog("Petarlib");
 
+
     @Override
     public void onInitializeClient() {
+        LogConfig.globalPrefix = "PetarLib";
         log.info("PetarLib initializing");
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
